251b3410b5c763b9aa526567876201f331dc56aa
Alex <eja777one@google.com>
2023-06-27 20:20:11 +0300

    change entity to resolve depenedency

--- src/products/dom/product.entity,ts.ts
+++ src/products/dom/product.entity,ts.ts
@@ -11,7 +11,7 @@ export class Product {
   @Column("character varying", { nullable: false })
   description: string;
 
-  @Column("character varying", { nullable: false })
+  @Column("character varying", { nullable: true })
   composition: string;
 
   @Column("character varying", { nullable: true })
